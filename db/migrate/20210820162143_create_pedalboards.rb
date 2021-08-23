3847ca360bd19553ebd2346ede2acb365e2848b0
Dan Beasley <dannyleebeasley@gmail.com>
2021-08-23 12:53:29 -0500

    migrated and seeded

--- db/migrate/20210820162143_create_pedalboards.rb
+++ db/migrate/20210820162143_create_pedalboards.rb
@@ -3,7 +3,7 @@ class CreatePedalboards < ActiveRecord::Migration[6.1]
     create_table :pedalboards do |t|
       t.string :name
       t.boolean :stereo
-      t.array :pedal_order
+      t.integer :pedal_order, array: true
       t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
