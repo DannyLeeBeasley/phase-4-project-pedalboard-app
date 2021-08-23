c16f2c90aa8e3c24239a35061fbb2a66d531672b
Dan Beasley <dannyleebeasley@gmail.com>
2021-08-23 13:05:30 -0500

    basic CRUD added to controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,42 @@
 class UsersController < ApplicationController
-    
+    wrap_parameters format: []
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        users = User.all 
+        render json: users, status: :ok 
+    end
+
+    def create 
+        user = User.create(user_params)
+        render json: user, status: :created
+    end
+
+    def update
+        user = find_user
+        user.update(user_params)
+        render json: user, status: :created 
+    end
+
+    def destroy
+        user = find_user
+        user.destroy
+        head :no_content
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :username, :email, :notes, :password_digest)
+    end
+
+    def find_user
+        User.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: user.errors.full_messages }, status: :not_found 
+    end
+
 end
+
