33bd26fea2ada556c67f235fe5c9922971eae6d1
Dan Beasley <dannyleebeasley@gmail.com>
2021-08-24 11:06:15 -0500

    login added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :password_digest, presence: true, uniqueness: true
+    has_secure_password
+
+    # def password=(value)
+    #     self.password_digest = BCrypt::Password.create(user_params[:password])
+    # end
+
+    # def authenticate(password)
+    #     BCrypt::Password.new(self.password_digest) == password
+    # end
+
 end
