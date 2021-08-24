f10ce109693b5ad16986690a312b2108afaccc42
Dan Beasley <dannyleebeasley@gmail.com>
2021-08-24 09:56:05 -0500

    validations added

--- app/models/pedalboard.rb
+++ app/models/pedalboard.rb
@@ -2,7 +2,8 @@ class Pedalboard < ApplicationRecord
   belongs_to :user
   has_many :pedalboard_pedals, dependent: :destroy
   has_many :pedals, through: :pedalboard_pedals
-  validates :stereo, presence: true
+  validates :name, presence: true
+  validates :stereo, presence: true, inclusion: { in: [ true, false ] }
   validates :pedal_order, presence: true
-  validates :user_id, presence: true
+  validates :user_id, presence: true, numericality: { only_integer: true }
 end
